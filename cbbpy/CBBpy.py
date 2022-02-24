af798e88f9652aab7c7426113a15c7a220e3447b
dacowan2 <dacowan@davidson.edu>
2022-02-23 18:07:09 -0600

    organization

--- cbbpy/CBBpy.py
+++ cbbpy/CBBpy.py
@@ -5,33 +5,57 @@ import requests as r
 BASE_URL = "https://www.espn.com/mens-college-basketball/"
 
 
-class CBBpy:
-    def __init__(self):
-        pass
+def scrape_game(id):
+    pass
 
-    def scrape_game(self, id):
-        pass
 
-    def _get_game_pbp(self, id):
-        pass
+def scrape_day(date):
+    pass
 
-    def _get_game_box(self, id):
-        pass
 
-    def _get_game_tm_stats(self, id):
-        pass
+def scrape_season(season):
+    pass
 
-    def scrape_day(self, date):
-        pass
 
-    def scrape_season(self, season):
-        pass
+def scrape_team_stats(team, season):
+    pass
 
-    def scrape_team_stats(self, team, season):
-        pass
 
-    def scrape_standings(self, season, conference):
-        pass
+def scrape_standings(season, conference):
+    pass
 
-    def scrape_rankings(self, season, week):
-        pass
+
+def scrape_conference_standings(conference, season):
+    pass
+
+
+def scrape_team_standings(team, season):
+    pass
+
+
+def scrape_rankings(season, week):
+    pass
+
+
+def scrape_schedule(season):
+    pass
+
+
+def scrape_conference_schedule(season, conference):
+    pass
+
+
+def scrape_team_schedule(team, season):
+    pass
+
+
+def _get_game_pbp(id):
+    pass
+
+
+def _get_game_box(id):
+    pass
+
+
+def _get_game_tm_stats(id):
+    pass
