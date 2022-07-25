106dadfe54baabf59641e9621e7ac50860f08ccb
dacowan2 <56355242+dcstats@users.noreply.github.com>
2022-07-24 19:32:54 -0500

    update

--- setup.py
+++ setup.py
@@ -3,10 +3,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='CBBpy',
-    version='0.1',
+    version='0.3',
     license='MIT',
     author="Daniel Cowan",
     author_email='dnlcowan37@gmail.com',
+    description='A Python-based web scraper for NCAA basketball.',
     packages=find_packages('src'),
     package_dir={'': 'src'},
     url='https://github.com/dcstats/CBBpy',
@@ -14,14 +15,9 @@ setup(
     install_requires=[
         'pandas',
         'numpy',
-        'html',
-        'datetime',
-        'dateutil',
+        'python-dateutil',
         'pytz',
-        'tqdm',
-        're',
-        'time',
-        'logging'
+        'tqdm'
       ],
 
 )
