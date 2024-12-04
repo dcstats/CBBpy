2f190bc21e46f795f58a5f4013a55ccb862b768f
dacowan2 <56355242+dcstats@users.noreply.github.com>
2024-12-03 20:51:57 -0600

    testing parameterization

--- tests/test_scraper.py
+++ tests/test_scraper.py
@@ -67,6 +67,11 @@ W_CONF_SCHEDULE = [
     (2022, "Southeastern Conference"),
     (2019, "West Coast Conference"),
 ]
+CONF_SEASONS = [
+    ('acc', 2017),
+    ('a10', 2022),
+    ('maac', 2019),
+]
 DATA_PATH = Path(__file__).parent / 'expected_data'
 
 
@@ -79,6 +84,7 @@ dtypes = {
     'home_id': str,
     'away_id': str,
     'opponent_id': str,
+    'player_id': str,
     'jersey_number': str,
     'pos': str,
     'starter': lambda x: x.lower() == 'true',
@@ -88,6 +94,7 @@ dtypes = {
     'shooter': converter,
     'assist_player': converter,
     'position': converter,
+    'home_record': converter,
     'away_record': converter,
     'play_desc': converter,
     'play_type': converter,
@@ -108,7 +115,7 @@ def load_expected_dataframe(file_path):
     return pd.read_csv(file_path, converters=dtypes)
 
 
-@pytest.mark.parametrize("func, ex_path, games", [
+@pytest.mark.parametrize("func, ex_path, data", [
     (ms.get_game_info, "mens_game_info", M_GAMES),
     (ms.get_game_boxscore, "mens_game_boxscore", M_GAMES),
     (ms.get_game_pbp, "mens_game_pbp", M_GAMES),
@@ -116,130 +123,96 @@ def load_expected_dataframe(file_path):
     (ws.get_game_boxscore, "womens_game_boxscore", W_GAMES),
     (ws.get_game_pbp, "womens_game_pbp", W_GAMES),
 ])
-def test_game_data(func, ex_path, games):
+def test_game_data(func, ex_path, data):
     expected_df = load_expected_dataframe(DATA_PATH / f"{ex_path}.csv")
     result_df = pd.DataFrame()
 
-    for g in games:
+    for g in data:
         d = func(str(g))
         result_df = pd.concat([result_df, d], ignore_index=True)
     
     pd.testing.assert_frame_equal(result_df, expected_df)
 
 
-def test_mens_player():
-    mens_expected_df = load_expected_dataframe(DATA_PATH / "mens_players.csv")
-    pl_ls = mens_expected_df.player_id.tolist()
+@pytest.mark.parametrize("func, game_type", [
+    (ms.get_player_info, "mens"),
+    (ws.get_player_info, "womens"),
+])
+def test_player(func, game_type):
+    expected_df = load_expected_dataframe(DATA_PATH / f"{game_type}_players.csv")
+    pl_ls = expected_df.player_id.tolist()
 
-    mens_result_df = pd.DataFrame()
-
-    for pl in pl_ls:
-        d = ms.get_player_info(pl)
-        mens_result_df = pd.concat([mens_result_df, d], ignore_index=True)
-
-    pd.testing.assert_frame_equal(mens_result_df, mens_expected_df)
-
-
-def test_womens_player():
-    womens_expected_df = load_expected_dataframe(DATA_PATH / "womens_players.csv")
-    pl_ls = womens_expected_df.player_id.tolist()
-
-    womens_result_df = pd.DataFrame()
+    result_df = pd.DataFrame()
 
     for pl in pl_ls:
-        d = ws.get_player_info(pl)
-        womens_result_df = pd.concat([womens_result_df, d], ignore_index=True)
+        d = func(pl)
+        result_df = pd.concat([result_df, d], ignore_index=True)
 
-    pd.testing.assert_frame_equal(womens_result_df, womens_expected_df)
+    pd.testing.assert_frame_equal(result_df, expected_df)
 
 
-def test_mens_team_schedule():
-    mens_result_df = pd.DataFrame()
-    mens_expected_df = load_expected_dataframe(DATA_PATH / "mens_team_schedule.csv")
+@pytest.mark.parametrize("func, ex_path, data", [
+    (ms.get_team_schedule, "mens_team_schedule", M_TEAM_SCHEDULE),
+    (ms.get_conference_schedule, "mens_conference_schedule", M_CONF_SCHEDULE),
+    (ws.get_team_schedule, "womens_team_schedule", W_TEAM_SCHEDULE),
+    (ws.get_conference_schedule, "womens_conference_schedule", W_CONF_SCHEDULE),
+])
+def test_schedule(func, ex_path, data):
+    result_df = pd.DataFrame()
+    expected_df = load_expected_dataframe(DATA_PATH / f"{ex_path}.csv")
 
-    for x in M_TEAM_SCHEDULE:
+    for x in data:
         sn, nm = x
-        d = ms.get_team_schedule(nm, sn)
-        mens_result_df = pd.concat([mens_result_df, d])
+        d = func(nm, sn)
+        result_df = pd.concat([result_df, d], ignore_index=True)
 
-    mens_result_df.reset_index(inplace=True, drop=True)
-
-    pd.testing.assert_frame_equal(mens_result_df, mens_expected_df)
+    pd.testing.assert_frame_equal(result_df, expected_df)
 
 
-def test_womens_team_schedule():
-    womens_result_df = pd.DataFrame()
-    womens_expected_df = load_expected_dataframe(DATA_PATH / "womens_team_schedule.csv")
-
-    for x in W_TEAM_SCHEDULE:
-        sn, nm = x
-        d = ws.get_team_schedule(nm, sn)
-        womens_result_df = pd.concat([womens_result_df, d])
-
-    womens_result_df.reset_index(inplace=True, drop=True)
-
-    pd.testing.assert_frame_equal(womens_result_df, womens_expected_df)
-
-
-def test_mens_conference_schedule():
-    mens_result_df = pd.DataFrame()
-    mens_expected_df = load_expected_dataframe(DATA_PATH / "mens_conference_schedule.csv")
-
-    for x in M_CONF_SCHEDULE:
-        sn, cf = x
-        d = ms.get_conference_schedule(cf, sn)
-        mens_result_df = pd.concat([mens_result_df, d])
-
-    mens_result_df.reset_index(inplace=True, drop=True)
-
-    pd.testing.assert_frame_equal(mens_result_df, mens_expected_df)
-
-
-def test_womens_conference_schedule():
-    womens_result_df = pd.DataFrame()
-    womens_expected_df = load_expected_dataframe(DATA_PATH / "womens_conference_schedule.csv")
-
-    for x in W_CONF_SCHEDULE:
-        sn, cf = x
-        d = ws.get_conference_schedule(cf, sn)
-        womens_result_df = pd.concat([womens_result_df, d])
-
-    womens_result_df.reset_index(inplace=True, drop=True)
-
-    pd.testing.assert_frame_equal(womens_result_df, womens_expected_df)
-
-
-def test_mens_range():
+@pytest.mark.parametrize("func, game_type", [
+    (ms.get_games_range, "mens"),
+    (ws.get_games_range, "womens"),
+])
+def test_range(func, game_type):
     start_date = "2022-03-01"
     end_date = "2022-03-31"
-    expected_info_df = load_expected_dataframe(DATA_PATH / "mens_game_info_range.csv")
-    expected_boxscore_df = load_expected_dataframe(DATA_PATH / "mens_game_boxscore_range.csv")
-    expected_pbp_df = load_expected_dataframe(DATA_PATH / "mens_game_pbp_range.csv")
-    result_info_df, result_boxscore_df, result_pbp_df = ms.get_games_range(start_date, end_date)
+    expected_info_df = load_expected_dataframe(DATA_PATH / f"{game_type}_game_info_range.csv")
+    expected_boxscore_df = load_expected_dataframe(DATA_PATH / f"{game_type}_game_boxscore_range.csv")
+    expected_pbp_df = load_expected_dataframe(DATA_PATH / f"{game_type}_game_pbp_range.csv")
+    result_info_df, result_boxscore_df, result_pbp_df = func(start_date, end_date)
     pd.testing.assert_frame_equal(result_info_df, expected_info_df)
     pd.testing.assert_frame_equal(result_boxscore_df, expected_boxscore_df)
     pd.testing.assert_frame_equal(result_pbp_df, expected_pbp_df)
 
 
-def test_womens_range():
-    start_date = "2022-03-01"
-    end_date = "2022-03-31"
-    expected_info_df = load_expected_dataframe(DATA_PATH / "womens_game_info_range.csv")
-    expected_boxscore_df = load_expected_dataframe(DATA_PATH / "womens_game_boxscore_range.csv")
-    expected_pbp_df = load_expected_dataframe(DATA_PATH / "womens_game_pbp_range.csv")
-    result_info_df, result_boxscore_df, result_pbp_df = ws.get_games_range(start_date, end_date)
-    pd.testing.assert_frame_equal(result_info_df, expected_info_df)
-    pd.testing.assert_frame_equal(result_boxscore_df, expected_boxscore_df)
-    pd.testing.assert_frame_equal(result_pbp_df, expected_pbp_df)
-
-
-def test_mens_season():
-    future_season = 2030
+@pytest.mark.parametrize("func", [
+    (ms.get_games_season),
+    (ws.get_games_season),
+])
+def test_season(func):
+    future_season = 3000
     with pytest.raises(InvalidDateRangeError, match="The start date must not be in the future."):
-        ms.get_games_season(future_season)
+        func(future_season)
 
 
-def test_womens_season():
-    future_season = 2030
-    with pytest.raises(InvalidDateRangeError, match="The start date must not be in the future."):
-        ws.get_games_season(future_season)
+@pytest.mark.parametrize("func, game_type", [
+    (ms.get_games_conference, "mens"),
+    (ws.get_games_conference, "womens"),
+])
+def test_conference_games(func, game_type):
+    expected_info_df = load_expected_dataframe(DATA_PATH / f"{game_type}_conference_info.csv")
+    expected_boxscore_df = load_expected_dataframe(DATA_PATH / f"{game_type}_conference_boxscore.csv")
+    expected_pbp_df = load_expected_dataframe(DATA_PATH / f"{game_type}_conference_pbp.csv")
+    result_info = pd.DataFrame()
+    result_box = pd.DataFrame()
+    result_pbp = pd.DataFrame()
+    
+    for conf, season in CONF_SEASONS:
+        info, box, pbp = func(conf, season)
+        result_info = pd.concat([result_info, info], ignore_index=True)
+        result_box = pd.concat([result_box, box], ignore_index=True)
+        result_pbp = pd.concat([result_pbp, pbp], ignore_index=True)
+        
+    pd.testing.assert_frame_equal(result_info, expected_info_df)
+    pd.testing.assert_frame_equal(result_box, expected_boxscore_df)
+    pd.testing.assert_frame_equal(result_pbp, expected_pbp_df)
